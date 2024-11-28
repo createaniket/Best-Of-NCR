39d0ed16e38996eb49f879d530b3980451337e42
Aniket Pratap Singh <aniketpratapsingh7310@gmail.com>
2024-11-28 19:00:36 +0530

    Oath impemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { GoogleLogin } from '@react-oauth/google';
 import Home from './components/Homepage/Home';
 
 function App() {
+
+  const handleLoginSuccess = (credentialResponse) => {
+    console.log("Login Success:", credentialResponse);
+  };
+
+  const handleLoginFailure = () => {
+    console.log("Login Failed");
+  };
+
   return (
     <div className="App">
       
       <Home />
+      <GoogleLogin
+        onSuccess={handleLoginSuccess}
+        onError={handleLoginFailure}
+      />
     
     </div>
   );
