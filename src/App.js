925cda8be384825f861d0676b15e801504cffea7
Aniket Pratap Singh <aniketpratapsingh7310@gmail.com>
2024-12-23 18:56:06 +0530

    reguar push one

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import { GoogleLogin } from '@react-oauth/google';
-import Home from './components/Homepage/Home';
+import { GoogleLogin } from "@react-oauth/google";
+import Home from "./components/Homepage/Home";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Privacypolicy from "./components/PrivacyPolicy/Privacypolicy";
 
 function App() {
-
   const handleLoginSuccess = (credentialResponse) => {
-    alert("Login Successful")
+    alert("Login Successful");
     console.log("Login Success:", credentialResponse);
   };
 
   const handleLoginFailure = () => {
-
-    alert("Login failed")
+    alert("Login failed");
     console.log("Login Failed");
   };
 
   return (
     <div className="App">
-      
-      <Home />
-      <GoogleLogin
-        onSuccess={handleLoginSuccess}
-        onError={handleLoginFailure}
-      />
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route
+            path="/login"
+            element={
+              <GoogleLogin
+                onSuccess={handleLoginSuccess}
+                onError={handleLoginFailure}
+              />
+            }
+          />
+
+<Route path="/privacy-policy" element={<Privacypolicy />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
